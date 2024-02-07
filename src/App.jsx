de4477ccdfa76b0e970c0243ec2bfe5a65afb0b6
Henry David Cadena <henry.cadena@tarjeta.click>
2024-02-07 17:04:12 -0500

    Completion Project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Brand from "./components/brand/Brand";
+import CTA from "./components/cta/CTA";
+import Navbar from "./components/navbar/Navbar";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
